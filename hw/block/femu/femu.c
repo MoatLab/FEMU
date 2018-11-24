2dc2c173402d0f69556de36b26586b7091799e16
Huaicheng Li <huaicheng@cs.uchicago.edu>
2018-11-24 12:20:46 -0600

    Restore one namespace

--- hw/block/femu/femu.c
+++ hw/block/femu/femu.c
@@ -1065,7 +1065,7 @@ static Property femu_props[] = {
     DEFINE_BLOCK_PROPERTIES(FemuCtrl, conf),
     DEFINE_PROP_STRING("serial", FemuCtrl, serial),
     DEFINE_PROP_UINT32("devsz_mb", FemuCtrl, memsz, 1024), /* Coperd: in MB */
-    DEFINE_PROP_UINT32("namespaces", FemuCtrl, num_namespaces, 4),
+    DEFINE_PROP_UINT32("namespaces", FemuCtrl, num_namespaces, 1),
     DEFINE_PROP_UINT32("queues", FemuCtrl, num_io_queues, 4),
     DEFINE_PROP_UINT32("entries", FemuCtrl, max_q_ents, 0x7ff),
     DEFINE_PROP_UINT8("max_cqes", FemuCtrl, max_cqes, 0x4),
