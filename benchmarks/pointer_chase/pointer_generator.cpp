670305606f9a0d39253b8faa3947b9bfd4a8dde0
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2020-07-04 17:00:39 -0400

    support for writing more than 4GB disk

--- benchmarks/pointer_chase/pointer_generator.cpp
+++ benchmarks/pointer_chase/pointer_generator.cpp
@@ -5,9 +5,6 @@
 
 #define DEBUG 0
 
-#define debug_print(fmt, ...) \
-            do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
-
 using namespace std;
 std::map <int, bool> pre_accessed_blocks;
 
