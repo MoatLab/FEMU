5336ad10c7281755a5b373ba2f128ea00cac5ae4
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2020-07-05 22:01:44 -0400

    simple script to read master output

---
+++ benchmarks/pointer_chase/scripts/result_reader.sh
@@ -0,0 +1,9 @@
+# Simple List script to list all times captured in a run
+
+if [[ "$#" -ne 1 ]]; then
+	echo "Usage: ./result_reader.sh [foldername] "
+	echo "foldername is the name of the folder that was generated with master_workload_generator.sh"
+	exit 1
+fi
+
+cat $foldername/*/*_time
