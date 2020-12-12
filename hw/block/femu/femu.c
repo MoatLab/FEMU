81d82de700de21ec42d3581196e5b0d913db5851
Huaicheng Li <huaicheng@cs.uchicago.edu>
2020-12-11 23:03:34 -0600

    Enable more QPs on the FEMU side by default to unlease more performance

--- hw/block/femu/femu.c
+++ hw/block/femu/femu.c
@@ -1150,7 +1150,7 @@ static Property femu_props[] = {
     DEFINE_PROP_STRING("serial", FemuCtrl, serial),
     DEFINE_PROP_UINT32("devsz_mb", FemuCtrl, memsz, 1024), /* Coperd: in MB */
     DEFINE_PROP_UINT32("namespaces", FemuCtrl, num_namespaces, 1),
-    DEFINE_PROP_UINT32("queues", FemuCtrl, num_io_queues, 1),
+    DEFINE_PROP_UINT32("queues", FemuCtrl, num_io_queues, 8),
     DEFINE_PROP_UINT32("entries", FemuCtrl, max_q_ents, 0x7ff),
     DEFINE_PROP_UINT8("multipoller_enabled", FemuCtrl, multipoller_enabled, 0),
     DEFINE_PROP_UINT8("max_cqes", FemuCtrl, max_cqes, 0x4),
