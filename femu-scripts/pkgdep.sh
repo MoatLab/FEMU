a5ad94672120795dac2c95185f59c3659f479ece
Huaicheng Li <huaicheng@cs.uchicago.edu>
2021-01-12 01:22:56 -0600

    Add Travis CI

--- femu-scripts/pkgdep.sh
+++ femu-scripts/pkgdep.sh
@@ -6,12 +6,15 @@ SYSTEM=`uname -s`
 
 if [[ -f /etc/debian_version ]]; then
 	# Includes Ubuntu, Debian
-    apt-get install -y gcc pkg-config git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja meson
+    apt-get install -y gcc pkg-config git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
     apt-get install -y libaio-dev
 
 	# Additional dependencies
 	apt-get install -y libnuma-dev
+    apt-get install -y ninja-build
 else
     echo "pkgdep: unsupported system type ($SYSTEM), please install QEMU depencies manually"
 	exit 1
 fi
+
+echo "===> Dependency installation ... Done!"
