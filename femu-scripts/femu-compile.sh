18a7700e830af3b8ad50631ca7a1163d5b1e55da
Huaicheng Li <lhcwhu@gmail.com>
2023-04-25 18:14:17 +0000

    Update FEMU compilation script

--- femu-scripts/femu-compile.sh
+++ femu-scripts/femu-compile.sh
@@ -4,7 +4,7 @@ NRCPUS="$(cat /proc/cpuinfo | grep "vendor_id" | wc -l)"
 
 make clean
 # --disable-werror --extra-cflags=-w --disable-git-update
-../configure --enable-kvm --target-list=x86_64-softmmu
+../configure --enable-kvm --target-list=x86_64-softmmu --enable-slirp
 make -j $NRCPUS
 
 echo ""
