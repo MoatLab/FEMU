4f2ab7bb9871bd4755f0a0f698c20a3d30da34e6
Huaicheng Li <lhcwhu@gmail.com>
2026-07-22 20:54:12 -0400

    femu/bbssd: move the remaining FTL inline accessors into the shared header
    Finish gathering the bbssd FTL's hot-path inline accessors in ftl-internal.h:
    the address-bounds check, the ppa/lpn validity predicates, and the channel /
    LUN / plane / block / line / page resource accessors move out of ftl.c alongside
    the mapping accessors already there. The datapath and GC call them exactly as
    before, now through the header. Pure relocation with no logic change.

--- hw/femu/bbssd/ftl-internal.h
+++ hw/femu/bbssd/ftl-internal.h
@@ -3,6 +3,74 @@
 
 #include "ftl.h"
 
+/* geometry / validity / resource accessors -- hot-path inlines */
+static inline void check_addr(int a, int max)
+{
+    ftl_assert(a >= 0 && a < max);
+}
+
+static inline bool valid_ppa(struct ssd *ssd, struct ppa *ppa)
+{
+    struct ssdparams *spp = &ssd->sp;
+    int ch = ppa->g.ch;
+    int lun = ppa->g.lun;
+    int pl = ppa->g.pl;
+    int blk = ppa->g.blk;
+    int pg = ppa->g.pg;
+    int sec = ppa->g.sec;
+
+    if (ch >= 0 && ch < spp->nchs && lun >= 0 && lun < spp->luns_per_ch && pl >=
+        0 && pl < spp->pls_per_lun && blk >= 0 && blk < spp->blks_per_pl && pg
+        >= 0 && pg < spp->pgs_per_blk && sec >= 0 && sec < spp->secs_per_pg)
+        return true;
+
+    return false;
+}
+
+static inline bool valid_lpn(struct ssd *ssd, uint64_t lpn)
+{
+    return (lpn < ssd->sp.tt_pgs);
+}
+
+static inline bool mapped_ppa(struct ppa *ppa)
+{
+    return !(ppa->ppa == UNMAPPED_PPA);
+}
+
+static inline struct ssd_channel *get_ch(struct ssd *ssd, struct ppa *ppa)
+{
+    return &(ssd->ch[ppa->g.ch]);
+}
+
+static inline struct nand_lun *get_lun(struct ssd *ssd, struct ppa *ppa)
+{
+    struct ssd_channel *ch = get_ch(ssd, ppa);
+    return &(ch->lun[ppa->g.lun]);
+}
+
+static inline struct nand_plane *get_pl(struct ssd *ssd, struct ppa *ppa)
+{
+    struct nand_lun *lun = get_lun(ssd, ppa);
+    return &(lun->pl[ppa->g.pl]);
+}
+
+static inline struct nand_block *get_blk(struct ssd *ssd, struct ppa *ppa)
+{
+    struct nand_plane *pl = get_pl(ssd, ppa);
+    return &(pl->blk[ppa->g.blk]);
+}
+
+static inline struct line *get_line(struct ssd *ssd, struct ppa *ppa)
+{
+    return &(ssd->lm.lines[ppa->g.blk]);
+}
+
+static inline struct nand_page *get_pg(struct ssd *ssd, struct ppa *ppa)
+{
+    struct nand_block *blk = get_blk(ssd, ppa);
+    return &(blk->pg[ppa->g.pg]);
+}
+
 /* mapping accessors (maptbl / rmap) -- hot-path inlines */
 static inline struct ppa get_maptbl_ent(struct ssd *ssd, uint64_t lpn)
 {
